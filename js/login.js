d4131f12960ca3eae2f36325deed7a2c5c5ea2f2
gs-px-l1-rs <102371231+gs-px-l1-rs@users.noreply.github.com>
2022-04-08 09:43:59 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,9 +1,9 @@
 // Gainsight PX Tag
-  (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
-      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
-    var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
-    var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-C80IAPOQUD9F-2");
+(function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+    (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
+var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
+var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
+})(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-C80IAPOQUD9F-2");
 
 // login elements and event listeners
 const loginForm = document.getElementById("login-form");
