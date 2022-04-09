05a2a294ee4f1bfc4203c18235eef33c862950ed
gs-px-l1-rs <102371231+gs-px-l1-rs@users.noreply.github.com>
2022-04-09 12:09:53 -0500

    new logout

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@
 var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
 var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
 })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-C80IAPOQUD9F-2");
+
+function logout() {
+    alert('Logging Out...');
+    document.cookie = 'apt.uid=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    document.cookie = 'apt.sid=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    window.location.href="login.html";
+  }
