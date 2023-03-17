1d761c10df326c204261536fc46d8e70fac3ab08
gs-px-l1-rs <102371231+gs-px-l1-rs@users.noreply.github.com>
2023-03-17 09:32:16 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ var config = {
     (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
 var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
 var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-})(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-C80IAPOQUD9F-2",config);
+})(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-C80IAPOQUD9F-2-1",config);
 
 /* (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
     (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
