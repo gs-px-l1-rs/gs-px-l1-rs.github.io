06108bdd6e51493a3be0e239bc95106b3333324a
gs-px-l1-rs <102371231+gs-px-l1-rs@users.noreply.github.com>
2023-10-06 11:20:57 -0500

    reidentify

--- js/script.js
+++ js/script.js
@@ -284,7 +284,7 @@ function round(x) {
  }
 
  function reReIdentify(){
-    window.aptrinsic('reset');
+    // window.aptrinsic('reset');
     aptrinsic("identify",
             {
             //User Fields
