bf08916ef5f90966382a17e30b148ff9e7522141
gs-px-l1-rs <102371231+gs-px-l1-rs@users.noreply.github.com>
2024-02-09 12:01:30 -0600

    Update script.js

--- product2partners/company-a/script.js
+++ product2partners/company-a/script.js
@@ -2,7 +2,7 @@
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-3JBWPZMJAMBI-2",{"iframeModeEnabled":true});
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-3JBWPZMJAMBI-2",{"iframeModeEnabled":true, "allowCrossDomain":true});
 
 function login() {
     const loginForm = document.getElementById("login-form");
