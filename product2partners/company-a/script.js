3ec5cade25d3a81c7b50a0385ae4add9983a05e6
gs-px-l1-rs <102371231+gs-px-l1-rs@users.noreply.github.com>
2025-03-12 09:51:44 -0500

    Update script.js

--- product2partners/company-a/script.js
+++ product2partners/company-a/script.js
@@ -2,7 +2,7 @@
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-UWIOTNCPHAJU-2",{"iframeModeEnabled":false, "allowCrossDomain":true, "fullDomainCookie":true});
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-UWIOTNCPHAJU-2",{"allowCrossDomain":true, "fullDomainCookie":true});
 
 function idUser(){
   aptrinsic('identify', {id: '4440a247be37c94c0e8bfbc613d3ba7b'});
