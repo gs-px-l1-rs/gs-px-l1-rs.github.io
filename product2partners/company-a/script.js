4faf1f1cb709912df20e825181c8fab0aefad8e9
gs-px-l1-rs <102371231+gs-px-l1-rs@users.noreply.github.com>
2025-03-12 07:15:10 -0500

    iframe test

--- product2partners/company-a/script.js
+++ product2partners/company-a/script.js
@@ -2,9 +2,10 @@
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-3JBWPZMJAMBI-2",{"iframeModeEnabled":true, "allowCrossDomain":true});
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-UWIOTNCPHAJU-2",{"iframeModeEnabled":false, "allowCrossDomain":true});
 
   function logout() {
     window.aptrinsic('reset');
     setTimeout(() => { window.location.href="login.html"; }, 500);
 }
+//3JBWPZMJAMBI
